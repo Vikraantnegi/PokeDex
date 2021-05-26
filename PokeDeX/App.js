76e4199df2a39c6b644c7c14a33202f3925ca419
vikrant <vikrant.negi74@gmail.com>
2021-05-26 22:43:06 +0530

    ADD Libraries

--- PokeDeX/App.js
+++ PokeDeX/App.js
@@ -1,38 +1,18 @@
 /* eslint-disable prettier/prettier */
-import React, {useState} from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import { store } from './src/redux/store';
 import { RootStackScreen } from './src/Navigation/Navigation';
-import SplashScreen from './src/Screens/SplashScreen';
 
 const App = () => {
-  const [time, setTimeUp] = useState(false);
-
-  setTimeout(() => {
-    setTimeUp(true);
-  }, 1500);
-
   return (
     <Provider store={store}>
       <NavigationContainer>
-        { time ? <RootStackScreen /> : <SplashScreen /> }
+       <RootStackScreen />
       </NavigationContainer>
     </Provider>
   );
 };
-
-const styles = StyleSheet.create({
-  AppContainer: {
-    paddingTop: 20,
-    height: '100%',
-    backgroundColor: 'white',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default App;
