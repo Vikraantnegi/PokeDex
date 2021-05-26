c60efb2c6ba567f0fa0b52a562cfcd911e82bdde
vikrant <vikrant.negi74@gmail.com>
2021-05-26 22:42:54 +0530

    Splash Screen Completed

--- PokeDeX/src/Screens/SplashScreen.js
+++ PokeDeX/src/Screens/SplashScreen.js
@@ -1,12 +1,47 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import {useSelector} from 'react-redux';
+import { UserName } from '../redux/features/authSlice';
 
-export default function SplashScreen() {
+export default function SplashScreen({navigation}) {
+    const username = useSelector(UserName);
+    console.log(username);
+    setTimeout(() => {
+        username ? navigation.navigate('Home') : navigation.navigate('Auth');
+    }, 2000);
     return (
-        <View>
-            <Text> Splash Screen </Text>
+        <View style={styles.AppContainer}>
+            <Animatable.Image
+                delay={500}
+                animation="bounceIn"
+                source={require('../assets/Pokedex.webp')}
+                style={styles.SplashImage}
+            />
         </View>
     );
 }
 
+const styles = StyleSheet.create({
+    AppContainer: {
+      paddingTop: 20,
+      position: 'relative',
+      height: '100%',
+      backgroundColor: '#46b5f9',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    SplashImage: {
+        width: '100%',
+        resizeMode: 'contain',
+    },
+    BottomImage: {
+        position: 'absolute',
+        top: 20,
+        width: '90%',
+        resizeMode: 'contain',
+    },
+  });
