061b60d7d51b87a2184ed232d54f1daf4d64a2b2
vikrant <vikrant.negi74@gmail.com>
2021-05-30 23:24:06 +0530

    PokeNews Assets Added

---
+++ PokeDeX/src/Data/DataFiles/NewsData.js
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+export default [
+    {
+        title: 'Showcasing Pokémon wearing Sunglasses on Pokémon TV',
+        image: require('../../assets/sunglasses-169-en.png'),
+        date: 'May 27, 2021',
+    },
+    {
+        title: 'Pokémon GO Fest 2021 : Five Years of Pokémon GO on July 17 and 18',
+        image: require('../../assets/pokemon-go-169.jpg'),
+        date: 'May 27, 2021',
+    },
+    {
+        title: 'Watch the Play! Pokémon Team Challenge Finals on May 28–30',
+        image: require('../../assets/2020-team-challenge-stream-updated-169.jpg'),
+        date: 'May 24, 2021',
+    },
+];
